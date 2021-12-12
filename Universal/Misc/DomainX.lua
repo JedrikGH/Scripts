6183f96096f49466ba049a9e14c6644b269123d4
Jedrik <80092075+JedrikGH@users.noreply.github.com>
2021-12-13 03:14:50 +0800

    Create DomainX.lua

---
+++ Universal/Misc/DomainX.lua
@@ -0,0 +1,6 @@
+--- CREATOR: shlex
+--- THREAD: https://v3rmillion.net/showthread.php?tid=1139087
+--- DISCORD: discord.gg/shlex
+
+
+loadstring(game:HttpGet(('https://shlex.dev/release/domainx/latest.lua'),true))()

07dfefcb21e9433daadd74b368c724c460724cc8
Jedrik <80092075+JedrikGH@users.noreply.github.com>
2021-12-13 03:20:24 +0800

    Create Impact X.lua

---
+++ Games/Pet Simulator X/Impact X.lua
@@ -0,0 +1,5 @@
+--- CREATOR: Boxking776
+--- THREAD: https://v3rmillion.net/showthread.php?tid=1148409
+--- DISCORD: https://discord.gg/UdAMuUeztT
+
+loadstring(game:HttpGet("https://schervi.wtf/ImpactXBeta.lua"))()

2d7fe2ffce23b36c19762542971b608a831d87b0
Jedrik <80092075+JedrikGH@users.noreply.github.com>
2021-12-13 03:29:19 +0800

    Create Crucified.lua

---
+++ Games/Simulators/Tapping Mania/Crucified.lua
@@ -0,0 +1,4 @@
+--- CREATOR: chrxstian
+--- THREAD: https://v3rmillion.net/showthread.php?tid=1140415
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/shroomsss/main/main/CrucifyHub.lua"))()

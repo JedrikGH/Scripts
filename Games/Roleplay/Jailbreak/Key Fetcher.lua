71ee1236e8b014ea66ef9ae268be96ee37dc0f60
Jedrik <80092075+JedrikGH@users.noreply.github.com>
2021-12-13 04:39:53 +0800

    Create Key Fetcher.lua

---
+++ Games/Roleplay/Jailbreak/Key Fetcher.lua
@@ -0,0 +1,4 @@
+--- CREATOR: crossliner
+--- THREAD: https://v3rmillion.net/showthread.php?tid=1144614
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/crossliner/jb-key-fetcher/master/Source.lua"))()

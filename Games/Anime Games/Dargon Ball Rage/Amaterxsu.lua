e02f9ebdc897cd7806b37ad786e40ca9d4b56ba2
Jedrik <80092075+JedrikGH@users.noreply.github.com>
2021-12-13 05:06:04 +0800

    Create Amaterxsu.lua

---
+++ Games/Anime Games/Dargon Ball Rage/Amaterxsu.lua
@@ -0,0 +1,6 @@
+--- CREATOR: Amaterxsu
+--- THREAD: https://v3rmillion.net/showthread.php?tid=1147978
+
+--- NOTE: Press 'P' to open/close the GUI
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/0mikami/Roblox-Scripts/main/AutoDBRObfuscated.lua"))()

b520734ff68ba99ba87c6b9a475e5b51821856e5
Jedrik <80092075+JedrikGH@users.noreply.github.com>
2021-12-13 05:04:40 +0800

    Create ManaHub.lua

---
+++ Games/Simulators/Anime Training Simulator/ManaHub.lua
@@ -0,0 +1,4 @@
+--- CREATOR: Mana64
+--- THREAD: https://v3rmillion.net/showthread.php?tid=1146697
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Mana42138/Manahub-Script-Hyper-804-was-here.lua/main/Hyper-is-still-here.lua"))()

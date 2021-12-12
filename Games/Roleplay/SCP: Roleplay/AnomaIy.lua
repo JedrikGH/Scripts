0de97677de5ee4f8928c4215e260862d8032de6d
Jedrik <80092075+JedrikGH@users.noreply.github.com>
2021-12-13 05:11:10 +0800

    Create AnomaIy.lua

---
+++ Games/Roleplay/SCP: Roleplay/AnomaIy.lua
@@ -0,0 +1,4 @@
+--- CREATOR: AnomaIy
+--- THREAD: https://v3rmillion.net/showthread.php?tid=1147682
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AnomalyRBLX/Database/main/Scripts/SCP%3A%20Roleplay"))();

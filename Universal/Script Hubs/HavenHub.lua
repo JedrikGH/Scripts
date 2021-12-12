e7b5d9f0c3c2c355e52b3d0463f7946d838c7901
Jedrik <80092075+JedrikGH@users.noreply.github.com>
2021-12-13 03:24:28 +0800

    Create HavenHub.lua

---
+++ Universal/Script Hubs/HavenHub.lua
@@ -0,0 +1,5 @@
+--- CREATOR: qwe
+--- THREAD: https://v3rmillion.net/showthread.php?tid=1148393
+--- DISCORD: https://discord.io/bittersweet
+
+loadstring(game:HttpGet("https://github.com/qertyuiop12345678/havenhub-v2.5/blob/main/havenhub.lua?raw=true"))()

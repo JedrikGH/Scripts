5fc73e8d1d55dfeb626fbbf58db66448c9d2c5ad
Jedrik <80092075+JedrikGH@users.noreply.github.com>
2021-12-13 04:26:56 +0800

    Create Mokuro Hub.lua

---
+++ Games/Anime Games/Blox Fruits/Mokuro Hub.lua
@@ -0,0 +1,4 @@
+--- CREATOR: 𝕩𝔻𝕖𝕡𝕣𝕖𝕤𝕤𝕚𝕠𝕟𝕩#6481
+--- DISCORD: https://discord.gg/dY264PSfVU
+
+loadstring(game:HttpGet"https://raw.githubusercontent.com/xDepressionx/Free-Script/main/AllScript.lua")()

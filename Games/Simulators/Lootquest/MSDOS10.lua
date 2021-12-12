d577154fe70d35dfc2a903cde5ffc82f3d116a62
Jedrik <80092075+JedrikGH@users.noreply.github.com>
2021-12-13 04:56:47 +0800

    Create MSDOS10.lua

---
+++ Games/Simulators/Lootquest/MSDOS10.lua
@@ -0,0 +1,4 @@
+--- CREATOR: MSDOS10
+--- THREAD: https://v3rmillion.net/showthread.php?tid=1146933
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/toskaman/fb/master/LQUpdate", true))()

1562f7db2ec19482dc34880aed782cdb01f27908
Jedrik <80092075+JedrikGH@users.noreply.github.com>
2021-12-13 05:00:40 +0800

    Create DexHub.lua

---
+++ Games/Roleplay/Prison Life/DexHub.lua
@@ -0,0 +1,5 @@
+--- CREATOR: GravityDex
+--- THREAD: https://v3rmillion.net/showthread.php?tid=1130113
+--- DISCORD: https://discord.io/dexhub
+
+loadstring(game:HttpGet("https://dexhub.xyz/loader.lua"))()

1d66d1fd3cdbdb0a06d7942fe3daef02b51ac41f
Jedrik <80092075+JedrikGH@users.noreply.github.com>
2021-12-13 03:19:06 +0800

    Create Ez Hub.lua

---
+++ Universal/Script Hubs/Ez Hub.lua
@@ -0,0 +1,5 @@
+--- CREATOR: CDXX
+--- THREAD: https://v3rmillion.net/showthread.php?tid=1087188
+--- DISCORD: https://discord.gg/tuWcU7Q
+
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/debug420/Ez-Industries-Launcher-Data/master/Launcher.lua'),true))()

8ad2a4b05b51619d1374ed8ea6c14bd11da3ccaa
Jedrik <80092075+JedrikGH@users.noreply.github.com>
2021-12-13 03:25:23 +0800

    Create V.G Hub.lua

---
+++ Universal/Script Hubs/V.G Hub.lua
@@ -0,0 +1,5 @@
+--- CREATOR: DekuDimz
+--- THREAD: https://v3rmillion.net/showthread.php?tid=1127420
+--- DISCORD: https://discord.gg/RHemSXfPpw
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/1201for/V.G-Hub/main/V.Ghub'))()
